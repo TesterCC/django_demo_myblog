ef093c4895df6378403a766fde714237aa893833
LYX <testerlyx@foxmail.com>
2017-07-30 22:41:53 +0800

    20170730_03, finish django blog simple demo, need add delete article function.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
 from django.contrib import admin
 
 from models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "pub_time")    # name must same in models.py
+    list_filter = ('pub_time',)
+    list_per_page = 3            # 按每页3个显示，自动分页
+
+admin.site.register(Article, ArticleAdmin)
